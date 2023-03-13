dbd408157014e1b2b3ec0bad94866da7337a7a04
aep <aepsaprul18@gmail.com>
2023-03-13 13:59:43 +0700

    update training page

--- resources/views/pages/training/index.blade.php
+++ resources/views/pages/training/index.blade.php
@@ -36,21 +36,21 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        @if (in_array("tambah", $current_data_navigasi))
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                  <button type="button" id="btn-create" class="btn bg-gradient-primary btn-sm pl-3 pr-3">
-                                    <i class="fas fa-plus"></i> Tambah
-                                  </button>
-                                  <a href="https://hcm.abata-printing.com/public/video_training/standard_penampilan_abata.mp4" target="_blank" id="btn-create" class="btn bg-gradient-primary btn-sm pl-3 pr-3">
-                                    Video Standard Penampilan
-                                  </a>
-                                  <a href="https://hcm.abata-printing.com/public/video_training/customer_service_excellence.m4v" target="_blank" id="btn-create" class="btn bg-gradient-primary btn-sm pl-3 pr-3">
-                                    Video CS Exellence
-                                  </a>
-                                </h3>
-                            </div>
-                        @endif
+                      <div class="card-header">
+                          <h3 class="card-title">
+                            @if (in_array("tambah", $current_data_navigasi))
+                              <button type="button" id="btn-create" class="btn bg-gradient-primary btn-sm pl-3 pr-3">
+                                <i class="fas fa-plus"></i> Tambah
+                              </button>
+                            @endif
+                            <a href="https://hcm.abata-printing.com/public/video_training/standard_penampilan_abata.mp4" target="_blank" id="btn-create" class="btn bg-gradient-primary btn-sm pl-3 pr-3">
+                              Video Standard Penampilan
+                            </a>
+                            <a href="https://hcm.abata-printing.com/public/video_training/customer_service_excellence.m4v" target="_blank" id="btn-create" class="btn bg-gradient-primary btn-sm pl-3 pr-3">
+                              Video CS Exellence
+                            </a>
+                          </h3>
+                      </div>
                         <div class="card-body">
                             <table id="example1" class="table table-bordered table-striped" style="font-size: 13px;">
                                 <thead>
